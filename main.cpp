23fda24b3cadd1b64054b36494b6055352ef5a52
sali-max <aliwordpad.gmil.com@gmail.com>
2025-03-17 07:33:03 +0330

    run snake

--- main.cpp
+++ main.cpp
@@ -5,31 +5,39 @@
 #include<unistd.h>
 using namespace std;
 
+//Global
+enum Direction {up, down, left, right, none};
 
 class snake
 {
     private:
         int size = 1;
-        enum Direction {up, down, left, right};
         Direction direction = up;
     public:
         void move_up()
         {
-            direction = up
+            direction = Direction::up;
         }
         void move_down()
         {
-            direction = down;
+            direction = Direction::down;
         }
         void move_left()
         {
-            direction = left;
+            direction = Direction::left;
         }
         void move_right()
         {
-            direction = right;
+            direction = Direction::right;
         }
-        
+        void move_none()
+        {
+            direction = Direction::none;
+        }
+        Direction whatis_direction()
+        {
+            return direction;
+        }
         void add_size()
         {
             size++;
@@ -37,40 +45,101 @@ class snake
 };
 
 
-void run(int h, int w)
+
+void run(int h, int w, snake object)
 {
+    int start_y = h / 2;    //Start Position
+    int start_x = w / 2;
+
     initscr();
+    noecho();
+    curs_set(0);
+    keypad(stdscr, TRUE); // Special key
 
     while (true)
-    {   
-        for (size_t i = 0; i < w; i++)
-        {
-            printw("#");
-        }
-        printw("\n");
-        for (int i=1; i < (h-1); i++) {
-            printw("#");
-            // cout << setw(w - 1) << "#";
-            mvprintw(i, w-1, "#");
+    {        
+        clear();
+        { //Frame
+            for (size_t i = 0; i < w; i++)
+            {
+                printw("#");
+            }
             printw("\n");
-        }
-        for (size_t i = 0; i < w; i++)
-        {
-            printw("#");
+            for (int i=1; i < (h-1); i++) {
+                printw("#");
+                // cout << setw(w - 1) << "#";
+                mvprintw(i, w-1, "#");
+                printw("\n");
+            }
+            for (size_t i = 0; i < w; i++)
+            {
+                printw("#");
+            }
+
         }
         
+        // snake Position
+        if(object.whatis_direction() == Direction::up)
+        {
+            start_y++;
+        }
+        else if(object.whatis_direction() == Direction::down)
+        {
+            start_y--;
+        }
+        else if(object.whatis_direction() == Direction::left)
+        {
+            start_x--;
+        }
+        else if(object.whatis_direction() == Direction::right)
+        {
+            start_x++;   
+        }
+
+        mvprintw(start_y, start_x, "@");    //Draw Snake head
+
+        
+        // Direction get and set Value
+        {
+            int key = getch();
+            if(key == 258) //UP
+            {
+                object.move_up();
+            }
+            else if(key == 259) //DOWN
+            {
+                object.move_down(); 
+            }
+            else if(key == 260) //LEFT
+            {
+                object.move_left();
+            }
+            else if(key == 261) //RIGHT
+            {
+                object.move_right();
+            }
+            else if(key == 27)
+            {
+                endwin();
+                exit(1);
+            }
+            else
+            {
+                object.move_none();
+            }
+        }
         
         refresh();
     }
 
 
-    refresh();
     endwin();
 }
 
 int main()
 {
-    // createFrame(12,25);
-    run(10,25);
+    snake player;
+
+    run(10,25, player);
     return 0;
 }
