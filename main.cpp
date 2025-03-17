61b1905cc27c2a9277b8bc034064d4edc939dc57
sali-max <aliwordpad.gmil.com@gmail.com>
2025-03-17 06:42:52 +0330

    Frame fix

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include<string>
 #include<iomanip>
-
+#include<ncurses.h>
+#include<unistd.h>
 using namespace std;
 
 
@@ -9,16 +10,26 @@ class snake
 {
     private:
         int size = 1;
+        enum Direction {up, down, left, right};
+        Direction direction = up;
     public:
         void move_up()
         {
-
+            direction = up
         }
         void move_down()
         {
-
+            direction = down;
         }
-
+        void move_left()
+        {
+            direction = left;
+        }
+        void move_right()
+        {
+            direction = right;
+        }
+        
         void add_size()
         {
             size++;
@@ -26,32 +37,40 @@ class snake
 };
 
 
-void createFrame(int h, int w)
+void run(int h, int w)
 {
-    for (size_t i = 0; i < w; i++)
-    {
-        printf("#");
+    initscr();
+
+    while (true)
+    {   
+        for (size_t i = 0; i < w; i++)
+        {
+            printw("#");
+        }
+        printw("\n");
+        for (int i=1; i < (h-1); i++) {
+            printw("#");
+            // cout << setw(w - 1) << "#";
+            mvprintw(i, w-1, "#");
+            printw("\n");
+        }
+        for (size_t i = 0; i < w; i++)
+        {
+            printw("#");
+        }
+        
+        
+        refresh();
     }
 
-    printf("\n");
-    
-    for (int i=1; i < (h-1); i++) {
-        printf("#");
-        cout << setw(w - 1) << "#";
-        printf("\n");
 
-    }
-
-    for (size_t i = 0; i < w; i++)
-    {
-        printf("#");
-    }
+    refresh();
+    endwin();
 }
-
-
 
 int main()
 {
-    createFrame(12,50);
+    // createFrame(12,25);
+    run(10,25);
     return 0;
 }
